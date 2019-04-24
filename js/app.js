1d6b18093df6e020c911d9dc2c4198104ba495d5
williams osunkwo <wosunkwo@gmail.com>
2019-04-24 12:15:44 -0700

    finished feauture 2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 //let sectionH1 = section.children('h1');
 const allPhotoGallery = [];
+var keywordVal = [];
 
 function PhotoGallery(image_url, title, description,keyword,horns){
   this.image_url = image_url;
@@ -17,14 +18,31 @@ $.get('../data/page-1.json', data =>{
     new PhotoGallery(element.image_url, element.title, element.description, element.keyword, element.horns)));
 
   let section = $('#photo-template');
+  let select = $('select');
+
+  for(let i=0; i<allPhotoGallery.length; i++){
+    if(!keywordVal.includes(allPhotoGallery[i].keyword)){
+      keywordVal.push(allPhotoGallery[i].keyword);
+      console.log(allPhotoGallery[i].keyword);
+      select.append(`<option>${allPhotoGallery[i].keyword}</option>`);
+    }
+   // select.append(`<option>${allPhotoGallery[i].keyword}</option>`);
+  }
 
   for(let i=0; i<allPhotoGallery.length; i++){
     section.append(`<img src="${allPhotoGallery[i].image_url}" alt = "${allPhotoGallery[i].keyword}">`);
     section.append(`<h2> ${allPhotoGallery[i].title}</h2>`);
-    /* section.append(`<p> ${allPhotoGallery[i].description}</p>`);*/
   }
-  /*
-  allPhotoGallery.forEach(element =>
-    section.append(`<h2> ${element.title}</h2>`)
-  );*/
+
+  $('select').on('change', function(){
+    $('section').empty();
+    for(let i=0; i<allPhotoGallery.length; i++){
+      if(allPhotoGallery[i].keyword === $(this).val()){
+        section.append(`<img src="${allPhotoGallery[i].image_url}" alt = "${allPhotoGallery[i].keyword}">`);
+        section.append(`<h2> ${allPhotoGallery[i].title}</h2>`);
+      }
+    }
+    console.log($(this).val());
+  });
 });
+
